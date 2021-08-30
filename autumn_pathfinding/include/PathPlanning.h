c021746593a8aae244af25d5cced0ea85da34b7a
Kleinrad <fabiankleinrad.fk@gmail.com>
2021-08-30 04:59:40 -0700

    pathfindingV1

---
+++ autumn_pathfinding/include/PathPlanning.h
@@ -0,0 +1,56 @@
+#ifndef PathPlaning_H
+#define PathPlaning_H
+
+class PathPlaning
+{
+public:
+  //Consturctor
+  PathPlaning(ros::NodeHandle n, int rMin, int rMax);
+
+  //Helper Methodes
+  bool isInitialized();
+
+  //PathPlaning Methods
+  void getPath(nav_msgs::OccupancyGrid g, geometry_msgs::Pose p, geometry_msgs::Point goal, pcl::PointCloud<pcl::PointXYZ> c, int nodeSpacing, int iteratons);
+
+private:
+  nav_msgs::OccupancyGrid Grid;
+  geometry_msgs::Pose Pose;
+  geometry_msgs::Point goal;
+  pcl::PointCloud<pcl::PointXYZ> cloud;
+  std::map<long, long> Tree;
+  std::pair<int, int> centerDelta;
+  int radiusCollisionMin;
+  int radiusCollisionMax;
+  double exponent;
+  const int pairingAdditiv = 10000;
+  //ROS Publisher
+  ros::Publisher pubNewNode;
+  ros::Publisher pubRandNode;
+  ros::Publisher pubPath;
+autum
+  std::pair<int, int> generateXrand(double goalDistance);
+  bool cellIsFree(int x, int y);
+  bool pathIsFree(long node1, long node2, int radius);
+  long getNearestNode(int x, int y, long goalNode);
+  long generateNewNode(long nearest, long random, int d);
+  double getPathLength(long node);
+  std::vector<long> getNearestNeighbors(long node, double range, long goalNode);
+  bool prevPathValid(long newPosNode, long goalNode, int nodeSpacing);
+
+  //Helper Methodes
+  geometry_msgs::PointStamped generatePoint(long node);
+  geometry_msgs::PoseStamped generatePose(long node);
+  nav_msgs::Path generatePath(long goalNode);
+  int gridIndex(int x, int y);
+  void setCenterDelta();
+  void ajustFloatingPoint();
+  long pairing(int x, int y);
+  double nodeDistance(long node1, long node2);
+  std::pair<int, int> depairing(long z);
+  //Mathemetical
+  int gschSum(int w);
+  int triangularRoot(int z);
+};
+
+#endif
---
+++ autumn_pathfinding/src/main.cpp
@@ -0,0 +1,70 @@
+#include "ros/ros.h"
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <sstream>
+#include "nav_msgs/OccupancyGrid.h"
+#include "nav_msgs/Path.h"
+#include "sensor_msgs/PointCloud2.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/PointStamped.h"
+#include <iostream>
+#include "PathPlanning.h"
+#include <pcl/common/projection_matrix.h>
+
+geometry_msgs::PoseStamped currentPose;
+nav_msgs::OccupancyGrid currentGrid;
+geometry_msgs::PointStamped goal;
+pcl::PointCloud<pcl::PointXYZ> cloud;
+bool activePathPlanning = false;
+PathPlaning *pp = nullptr;
+
+void gridcallback(const nav_msgs::OccupancyGrid::ConstPtr &msg)
+{
+  nav_msgs::OccupancyGrid grid = *msg;
+  currentGrid = grid;
+  if (!activePathPlanning)
+  {
+    activePathPlanning = true;
+    pp->getPath(currentGrid, currentPose.pose, goal.point, cloud, 12, 8000);
+    activePathPlanning = false;
+  }
+}
+
+void pathcallback(const geometry_msgs::PoseStamped::ConstPtr &msg)
+{
+  geometry_msgs::PoseStamped poseData = *msg;
+  currentPose = poseData;
+  //std::cout << poseData.pose.position << std::endl;
+}
+
+void pointClickedcallback(const geometry_msgs::PointStamped::ConstPtr &msg)
+{
+  geometry_msgs::PointStamped goalPoint = *msg;
+  goal = goalPoint;
+  //          OccupancyGrid   ZED Position     GOAL Position   D    i
+  pp->getPath(currentGrid, currentPose.pose, goalPoint.point, cloud, 12, 8000);
+}
+
+void cloud2dcallback(const sensor_msgs::PointCloud2::ConstPtr &msg)
+{
+  pcl::PCLPointCloud2 pcl_pc2;
+  pcl_conversions::toPCL(*msg, pcl_pc2);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::fromPCLPointCloud2(pcl_pc2, *temp_cloud);
+  cloud = *temp_cloud;
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "explorator");
+  ros::NodeHandle n;
+  pp = new PathPlaning(n, 4, 4);
+  ros::Subscriber gridSub = n.subscribe("/zed/map", 1, &gridcallback);
+  ros::Subscriber pathSub = n.subscribe("/zed/zed_node/pose", 1, &pathcallback);
+  ros::Subscriber goalSub = n.subscribe("/clicked_point", 1, &pointClickedcallback);
+  ros::Subscriber cloudSub = n.subscribe("/zed/cloud_map", 1, &cloud2dcallback);
+
+  ros::spin();
+  return 0;
+}
